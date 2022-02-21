b06cd14ea4e654643704d44dd604c3cfaa7b8bc0
Harshini9500 <gsriharshini7981@gmail.com>
2022-02-21 10:59:22 +0530

    modified files are being commited

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
+
+function updateRate() 
+{
+
+    var rateval = document.getElementById("rate").value;
+
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+   
+
+
+    var principal = document.getElementById("principal").value;
+
+    var rate = document.getElementById("rate").value;
+
+    var years = document.getElementById("years").value;
+
+    var amount = principal * years * rate /100;
+
+    var year = new Date().getFullYear()+parseInt(years);
+
+
+    var str="If you deposit <mark>"+principal+"</mark>,<br/>at an interest rate of <mark>"+rate+"%</mark>. <br/>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>";
+
+    document.getElementById("result").innerHTML=str;
     
 }
+
+
+
+
+
+
+
+
+
+
+
         
